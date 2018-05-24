776b4d97f7255be20ebbbf21853d0a42c1d7967b
ash975 <ash975@live.com>
2018-05-24 15:13:07 +0800

    add mannual

--- material_hunter/nhk_web_easy.py
+++ material_hunter/nhk_web_easy.py
@@ -11,21 +11,26 @@ URL = "https://www3.nhk.or.jp/news/easy/news-list.json"
 ARTICLE_DIR = "../material"
 URL_EASY = "https://www3.nhk.or.jp/news/easy"
 
+# auto
+TODAY = str(date.today())
+# manual
+# TODAY = "2018-05-22"
+
 send_header = {
-"user-agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36",
-"accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-"accept-encoding": "gzip, deflate, br",
-"accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,ja;q=0.6"
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36",
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+    "accept-encoding": "gzip, deflate, br",
+    "accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,ja;q=0.6"
 }
 
 response = requests.request(method="GET", url=URL, headers=send_header)
 
-article_today_dir = ARTICLE_DIR + "/" + str(date.today()) + "/news"
+article_today_dir = ARTICLE_DIR + "/" + TODAY + "/news"
 if not os.path.exists(article_today_dir):
     os.makedirs(article_today_dir)
 
 article_list = response.json()
-article_today = article_list[0].get(str(date.today()))
+article_today = article_list[0].get(TODAY)
 
 for article in article_today:
     article_id = str(article.get('news_id'))
@@ -83,6 +88,3 @@ for article in article_today:
         sys.exit(0)
 
     time.sleep(5)
-
-
-
